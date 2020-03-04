060c30b45c0a7489cb57582e4d7d065384f2c93d
rhmn68 <auliar68@gmail.com>
2020-03-04 09:10:12 +0700

    Sign Up Activity

---
+++ app/src/main/java/com/rahmanaulia/firebaseauth/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.rahmanaulia.firebaseauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(tbSignUp)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+    }
+}
