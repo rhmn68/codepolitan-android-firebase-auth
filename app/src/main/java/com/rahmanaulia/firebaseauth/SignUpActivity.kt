2803f49fe093ca8eabd09d7bde09d03486649144
rhmn68 <auliar68@gmail.com>
2020-03-04 14:39:41 +0700

    Sign-Up with email and pass

--- app/src/main/java/com/rahmanaulia/firebaseauth/SignUpActivity.kt
+++ app/src/main/java/com/rahmanaulia/firebaseauth/SignUpActivity.kt
@@ -3,6 +3,9 @@ package com.rahmanaulia.firebaseauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -14,12 +17,63 @@ class SignUpActivity : AppCompatActivity() {
         initActionBar()
 
         btnSignUp.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            val email = etEmailSignUp.text.toString().trim()
+            val pass = etPasswordSignUp.text.toString().trim()
+            val confirmPass = etConfirmPasswordSignUp.text.toString().trim()
+
+            CustomDialog.showLoading(this)
+            if (checkValidation(email, pass, confirmPass)){
+                registerToServer(email, pass)
+            }
         }
 
         tbSignUp.setNavigationOnClickListener {
             finish()
         }
+    }
+
+    private fun registerToServer(email: String, pass: String) {
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener{task ->
+                CustomDialog.hideLoading()
+                if (task.isSuccessful){
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                }
+            }
+            .addOnFailureListener{
+                CustomDialog.hideLoading()
+                Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkValidation(email: String, pass: String, confirmPass: String): Boolean {
+        if (email.isEmpty()){
+            etEmailSignUp.error = "Please field your email"
+            etEmailSignUp.requestFocus()
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmailSignUp.error = "Please use valida email"
+            etEmailSignUp.requestFocus()
+        }else if (pass.isEmpty()){
+            etPasswordSignUp.error = "Please field your password"
+            etPasswordSignUp.requestFocus()
+        }else if (confirmPass.isEmpty()){
+            etConfirmPasswordSignUp.error = "Please field your confirm password"
+            etConfirmPasswordSignUp.requestFocus()
+        }else if (pass != confirmPass){
+            etPasswordSignUp.error = "Your pass didnt match"
+            etConfirmPasswordSignUp.error = "Your confirm pass didnt match"
+
+            etPasswordSignUp.requestFocus()
+            etConfirmPasswordSignUp.requestFocus()
+        }else{
+            etPasswordSignUp.error = null
+            etConfirmPasswordSignUp.error = null
+            return true
+        }
+        CustomDialog.hideLoading()
+        return false
     }
 
     private fun initActionBar() {
