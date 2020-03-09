e1604c312e075d4f71e590727e5d35480a385407
rhmn68 <auliar68@gmail.com>
2020-03-09 11:04:05 +0700

    Google Sign-In

--- app/src/main/java/com/rahmanaulia/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/rahmanaulia/firebaseauth/MainActivity.kt
@@ -3,12 +3,17 @@ package com.rahmanaulia.firebaseauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient:GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,9 @@ class MainActivity : AppCompatActivity() {
 
         btnLogout.setOnClickListener {
             auth.signOut()
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                Toast.makeText(this, "Success Logout", Toast.LENGTH_SHORT).show()
+            }
             startActivity(Intent(this,AuthActivity::class.java))
             finish()
         }
@@ -33,5 +41,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun initFirebaseAuth() {
         auth = FirebaseAuth.getInstance()
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 }
--- app/src/main/java/com/rahmanaulia/firebaseauth/SignInActivity.kt
+++ app/src/main/java/com/rahmanaulia/firebaseauth/SignInActivity.kt
@@ -5,14 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    companion object{
+        const val RC_SIGN_IN = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +47,28 @@ class SignInActivity : AppCompatActivity() {
 
         tbSignIn.setNavigationOnClickListener {
             finish()
+        }
+
+        btnGoogleSignIn.setOnClickListener {
+            val signIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signIntent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            CustomDialog.showLoading(this)
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                fireBaseAuth(credential)
+            }catch (e: ApiException){
+                CustomDialog.hideLoading()
+                Toast.makeText(this, "Sign-In Cancelled", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -63,6 +96,14 @@ class SignInActivity : AppCompatActivity() {
 
     private fun initFirebaseAuth() {
         auth = FirebaseAuth.getInstance()
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     private fun checkValidation(email: String, pass: String): Boolean {
